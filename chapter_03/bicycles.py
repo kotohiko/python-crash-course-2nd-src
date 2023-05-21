a44d3b04980f948c08bd493204b434044561adca
kotohiko <tachibana.kotohiko@gmail.com>
2023-05-21 23:50:00 +0800

    格式优化

--- chapter_03/bicycles.py
+++ chapter_03/bicycles.py
@@ -1,5 +1,6 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 message = f"My first bicycle was a {bicycles[0].title()}."
 
-print(bicycles[-1].title())  # 负数索引真是太秀了
+# 负数索引真是太秀了
+print(bicycles[-1].title())
 print(message)
--- chapter_03/cars.py
+++ chapter_03/cars.py
@@ -1,5 +1,6 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 print(cars)
 
+cars.sort()
 cars.reverse()
 print(cars)
